1c7ab429b55f2a3e4b9f22463f0c4d1cdd4565ca
Luis Ayala <ayalaluis638@gmail.com>
2024-05-23 09:23:52 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,53 @@
+
+let indexPregunta = 0;
+cargarPregunta(indexPregunta);
+let puntaje = 0;
+
+function cargarPregunta (index){
+    objetoPregunta = preguntas[index]
+    opciones = [...objetoPregunta.respuestaIncorrecta]
+    opciones.push(objetoPregunta.respuestaCorrecta)
+    for (let i = 0; i<4; i++){
+        opciones.sort(() => Math.random() -0.5);
+    }
+    
+    document.getElementById("pregunta").innerHTML = objetoPregunta.pregunta
+    document.getElementById("opcion1").innerHTML = opciones[0]
+    document.getElementById("opcion2").innerHTML = opciones[1]
+    document.getElementById("opcion3").innerHTML = opciones[2]
+    document.getElementById("opcion4").innerHTML = opciones[3]
+}
+
+async function seleccionarOpcion (index){
+    
+    let validez = opciones[index] == objetoPregunta.respuestaCorrecta;
+    
+    if (validez) {
+        await Swal.fire({
+            title: "Felicidades",
+            text: "La respuesta es correcta",
+            icon: "success"
+          });
+          puntaje++;
+    }else{
+        await Swal.fire({
+            title: "Respues incorrecta",
+            text: `La respuesta correcta es: "${objetoPregunta.respuestaCorrecta}"`,
+            icon: "error"
+          });
+    }
+
+   indexPregunta++;
+   if (indexPregunta >= preguntas.length){
+    indexPregunta= 0;
+    await Swal.fire({
+        title: "Juego terminado",
+        text: `Tu puntaje es: "${puntaje}"/${preguntas.length}`,
+        icon: "error"
+      });
+      indexPregunta = 0;
+      puntaje = 0;
+   }
+   cargarPregunta(indexPregunta)
+
+}
---
+++ js/preguntas.js
@@ -0,0 +1,12 @@
+let preguntas = [
+    {
+        pregunta : "Los dientes de leche no se cuidan ni se lavan porque se van a caer",
+        respuestaCorrecta: "No, los dientes de leche son igual de importantes que los dientes normales, su cuidado es fundamental para la prevencion de futuras enfermedades",
+        respuestaIncorrecta:["Es correcto a los dientes de leche no hay que darle mucha importancia mientras antes los saquemos mucho mejor", "Es correcto es mejor no cuidarlos los importantes son los dientes normales", "Es correcto lo mejor es comer dulces y aprovechar nuestros dientes de leche porque cuando crezcan los nuevos no vamos a poder"],
+    },
+    {
+        pregunta:"¿Cuanto tiempo dura el lavado de dientes?",
+        respuestaCorrecta: "2-5 minutos",
+        respuestaIncorrecta:["1 hora", "10 segundos","30 minutos"],
+    }
+]

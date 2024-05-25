69be28ee3348603ea28ab220efeba4320673eaf9
Luis Ayala <ayalaluis638@gmail.com>
2024-05-25 12:42:18 -0300

    Agregando inicio

--- html/js/preguntas.js
+++ html/js/preguntas.js
@@ -2,7 +2,7 @@ let preguntas = [
     {
         pregunta : "Los dientes de leche no se cuidan ni se lavan porque se van a caer",
         respuestaCorrecta: "No, los dientes de leche son igual de importantes que los dientes normales, su cuidado es fundamental para la prevencion de futuras enfermedades",
-        respuestaIncorrecta:["Es correcto a los dientes de leche no hay que darle mucha importancia mientras antes los saquemos mucho mejor", "Es correcto es mejor no cuidarlos los importantes son los dientes normales", "Es correcto lo mejor es comer dulces y aprovechar nuestros dientes de leche porque cuando crezcan los nuevos no vamos a poder"],
+        respuestaIncorrecta:["Es correcto, a los dientes de leche no hay que darle mucha importancia mientras antes los saquemos mucho mejor", "Es correcto es mejor no cuidarlos los importantes son los dientes normales", "Es correcto, lo mejor es comer dulces y aprovechar nuestros dientes de leche porque cuando crezcan los nuevos no vamos a poder"],
     },
     {
         pregunta:"¿Cuanto tiempo dura el lavado de dientes?",
@@ -19,5 +19,10 @@ let preguntas = [
         respuestaCorrecta:"No pueden nacer con caries pero si con alguna malformacion",
         respuestaIncorrecta:["Si pueden nacer con caries", "Si tienes una mala higiene te pueden salir con caries", "Las caries se quedan pegadas a las encias por ende se traspasan a los nuevos dientes"],
     },
- 
+    {
+        pregunta:"Antes de lavarse los dientes es mejor mojar el cepillo",
+        respuestaCorrecta:"No, mojando el cepillo solo conseguiras que la pasta de dientas pierda su fuerza y no consiga arrastrar bien la suciedad ",
+        respuestaIncorrecta: ["Si, esta bien","Primero mojamos el cepillo luego agregamos la pasta de dientes", "todas son correctas" ],
+
+    }
 ]

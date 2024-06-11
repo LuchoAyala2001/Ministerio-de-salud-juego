ded31953b892fb293877e6bd9cdf1e88a4da8524
Luis Ayala <ayalaluis638@gmail.com>
2024-06-10 22:30:46 -0300

    Agregando rpreguntas y cambiando fuente de preguntas y  respuestas

--- html/js/preguntas.js
+++ html/js/preguntas.js
@@ -1,6 +1,6 @@
 let preguntas = [
     {
-        pregunta : "Los dientes de leche no se cuidan ni se lavan porque se van a caer",
+        pregunta : "Los dientes de leche no se cuidan ni se lavan porque se van a caer.",
         respuestaCorrecta: "No, los dientes de leche son igual de importantes que los dientes normales, su cuidado es fundamental para la prevencion de futuras enfermedades",
         respuestaIncorrecta:["Es correcto, a los dientes de leche no hay que darle mucha importancia mientras antes los saquemos mucho mejor", "Es correcto es mejor no cuidarlos los importantes son los dientes normales", "Es correcto, lo mejor es comer dulces y aprovechar nuestros dientes de leche porque cuando crezcan los nuevos no vamos a poder"],
     },
@@ -24,5 +24,57 @@ let preguntas = [
         respuestaCorrecta:"No, mojando el cepillo solo conseguiras que la pasta de dientas pierda su fuerza y no consiga arrastrar bien la suciedad ",
         respuestaIncorrecta: ["Si, esta bien","Primero mojamos el cepillo luego agregamos la pasta de dientes", "todas son correctas" ],
 
+    },
+    {
+        pregunta: "¿Al lavarse los dientes, es necesario lavarse la lengua?",
+        respuestaCorrecta: "Si, es necesario lavarse la lengua ya que ayuda a eliminar las bacterias que causan mal aliento y contribuye a una buena higiene bucal ",
+        respuestaIncorrecta: ["No, no es necesario lvarse la lengua al cepillarse los dientes, ya que la lengua no acumula bacterias", "Es opcional lavarse la lengua porque los enjuagues bucales arreglan todo","Solo tenes que lavarte la lengua si te duele la boca sino no es necesario"]
+    },
+    {
+        pregunta: "¿Cuantas veces al año es necesario ir al dentista?",
+        respuestaCorrecta: "Es importante ir al dentista dos veces al año para asegurarse que tus dientes estan sanos",
+        respuestaIncorrecta: ["No necesitas ir al dentista si te cepillas los dientes todos los dias", "Tenes que ir al dentista una vez al mes para mantener tus dientes limpios", "Solo tenes que ir al dentista si tenes un dolor de muelas"]
+    },
+    {
+        pregunta:"¿En el dia cuantas veces al dia hay que cepillarse",
+        respuestaCorrecta:"Es importante cepillarse los dientes 3 veces al dia: Por la mañana, despues del almuerzo, y antes de ir a dormir",
+        respuestaIncorrecta:["Solo necesitas cepillarte los dientes una vez al dia", "Debes cepillarte los dientes cada vez que comes algo", "Solo necesitas cepillarte los dientes si sentis que estan sucios"]
+    },
+    {
+        pregunta:"¿El cepillo de dientes hay que cambiarlo?",
+        respuestaCorrecta:"Si, es necesario cambiar el cepillo de dientes. Los dentistas lo recomiendan cada 3 meses",
+        respuestaIncorrecta:["Podes cambiar el cepillo de dientes solo cuando te acordas", "Solo tenes que cambiar el cepillo de dientes si se rompe o lo perdes", "El cepillo de dientes es para toda la vida"]
+    },
+    {
+        pregunta: "¿Porque son importantes los dientes?",
+        respuestaCorrecta:"Los dientes son importantes porque ayudan a masticar la comida, hablar claramente y mantener una sonrisa bonita",
+        respuestaIncorrecta: ["Los dientes no tienen ninguna funcion especial", "Los dientes solo son importantes para masticar chicles", "Los dientes solo son importantes para los adultos"]
+    },
+    {
+        pregunta:"¿Cuantos dientes tenemos?",
+        respuestaCorrecta:"Tenemos 32 de adultos y 20 dientes de leche cuando somos niños",
+        respuestaIncorrecta:["Tenemos 50 dientes", "Tenemos 10 dientes en total", "Tenemos 100 dientes en total"]
+    },
+    {
+        pregunta:"¿Es necesario lavarse con fuerza?",
+        respuestaCorrecta:"No, hay que cepillarse suavemente para cuidar los dientes y cuidar nuestras encias",
+        respuestaIncorrecta:["No importa la fuerza con la que te laves, lo importante es usar mucha pasta de dientes", "Hay que lavarse con fuerza para sacarse la mancha de los dientes", "Si lavamos con fuerza podemos romper el cepillo de dientes"]
+    },
+    {
+        pregunta:"¿Cual es la funcion de cada diente?",
+        respuestaCorrecta:"Los dientes incisivos cortan la comida, los caninos la desgarran, y los molares y premolares la muelen",
+        respuestaIncorrecta:["Los dientes de arriba se usan para morder y los de abajo para masticar","Los dientes solo sirven para comer caramelos y dulces","Los dientes solo sirven para sonreir bonito y no tienen ninguna otra funcion"]
+    },
+    {
+        pregunta:"¿Que es el enjuague bucal?¿Es mejor eso que cepillrse los dientes?",
+        respuestaCorrecta:"El enjuague bucal es un liquido que ayuda a matar las bacterias en la boca, pero no es mejor que cepillarse los dientes",
+        respuestaIncorrecta:["El enjuague bucal es un reemplazo del cepillo de dientes y es mas facil de usar", "El enjuague bucal es agua y no tiene ninguna funcion especial en comparacion del cepillado","El enjuague bucal sirve para tener mas blancos los dientes, es mejor que cepillarse"]
+    },
+    {
+        pregunta:"¿Porque hay que lavarse los dientes?",
+        respuestaCorrecta:"Hay que lavarse los dientes para mantenerlos fuertes y sanos, para evitar caries y enfermedades",
+        respuestaIncorrecta:["Hay que lavarse los dientes para que esten blancos y brillantes", "Porque sino los dientes van a hacer una huelga de hambre y se van a caer","Solo hay que lavarlos si sentimos la boca sucia sino no es necesario"]
     }
+
+
 ]
